9f4838bd11817f03e464e6f4cec1900eb4ebcc05
Rohan Deshmukh <rd261@duke.edu>
2025-11-18 12:52:08 -0500

    Commented out print statements

--- rocket_B.py
+++ rocket_B.py
@@ -53,11 +53,11 @@ def approx(sim_time, t_step, h_0, v_0):
         h_plot[i] = h + t_step*v
         h = h_plot[i]
         if h >= 400000 and has_printed == False:
-            print(f"to get to 400km, rocket has used {(wet_mass - m(t))/(fuel_weight)*100}% of the fuel")
-            print(f"current mass is {m(t)}")
-            print(f"dry mass is {dry_mass}")
-            print(f"wet mass is {wet_mass}")
-            print(f"fuel weight is {fuel_weight}")
+            # print(f"to get to 400km, rocket has used {(wet_mass - m(t))/(fuel_weight)*100}% of the fuel")
+            # print(f"current mass is {m(t)}")
+            # print(f"dry mass is {dry_mass}")
+            # print(f"wet mass is {wet_mass}")
+            # print(f"fuel weight is {fuel_weight}")
             has_printed = True
         t_plot[i] = t_plot[i-1] + t_step
         t += t_step
