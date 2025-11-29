9fcdfabd4ec5df12bc3370c799549f293619839b
Reid Korva <rck42@duke.edu>
2025-11-29 00:21:22 -0800

    Start of final simulation full draft

---
+++ A_milestone_final_simulation.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+from b_ms.constants import *
+from Runge_Kutta_Integration_Interface import *
+from Acceleration_Funcs_for_Runge_Kutta_Method import *
+
+def final_simulation(altitude, velocity, timestep, orbital_decay_time,final_burn_time):
+    od_time = np.arange(0,orbital_decay_time,timestep)
+    fb_time = np.arange(0,final_burn_time,timestep)
+    pos = np.zeros((len(od_time)+len(fb_time),2))
+    vel = np.zeros((len(od_time)+len(fb_time),2))
+    
+    pos[0] = [0,altitude+R_EARTH]
+    vel[0] = [velocity,0]
+
+    current_time = 0
+    while current_time < orbital_decay_time:
+        pos[current_time], vel[current_time] = Runge_Kutta(orbital_decay_accel,pos,vel,timestep)
