56ca9d7547a94569dabd69134f772f1025eddad2
Pierson Jones <paj14@duke.edu>
2025-12-02 12:30:07 -0500

    Fixed full simulation file and reentry. Function now hits ground during reentry stage.

--- b_ms/reentry_B_.py
+++ b_ms/reentry_B_.py
@@ -34,61 +34,59 @@ def second_order_ex(x0, y0, vx0, vy0, tstep, max_steps):
     mu = G * M_EARTH
     status = ''
     for i in range(max_steps):
-        t_list.append(t)
-        x_list.append(x)
-        y_list.append(y)
-        vx_list.append(vx)
-        vy_list.append(vy)
         # Set distance from Earth's center
         r = math.sqrt(x*x + y*y)
         # Set altitude based off of Earth's center
         altitude = r - R_EARTH
-        vmag = math.sqrt(vx**2+vy**2)
-        rho = RHO_LEO*np.exp(-(altitude/7500))
-        Fd = .5*rho*(vmag**2)*(C_D_ISS*AREA_ISS)
         # End the function when the mass hits the ground
         if altitude <= 0:
             status = 'hit ground'
             tarray = np.array(t_list)
             posarray = np.column_stack((x_list, y_list))
             varray = np.column_stack((vx_list, vy_list))
-            return tarray, posarray, varray, status
-        # Update x for each step
+            return posarray, varray, tarray, status
+        vmag = math.sqrt(vx**2 + vy**2)
+        rho = RHO_0*np.exp(-altitude/H_0)
+        if vmag < 1e-12:
+            ax_drag = 0
+            ay_drag = 0
         else:
-            ax_gravity = -mu * x /(r**3)
-            ay_gravity = -mu * y /(r**3)
+            Fd = 0.5*rho*(vmag**2)*C_D_ISS*AREA_ISS
             ax_drag = -(Fd / (vmag * total_mass)) * vx
             ay_drag = -(Fd / (vmag * total_mass)) * vy
-            ax = ax_gravity + ax_drag
-            ay = ay_gravity + ay_drag
-            vx = vx + ax * (tstep)
-            vy = vy + ay * (tstep)
-            x = x + vx * (tstep)
-            y = y + vy * (tstep)
-            t += tstep
-        status = 'orbiting or escaped orbit'
+        # Update x for each step
+        ax_gravity = -mu * x / (r**3)
+        ay_gravity = -mu * y / (r**3)
+        ax = ax_gravity + ax_drag
+        ay = ay_gravity + ay_drag
+        vx = vx + ax * tstep
+        vy = vy + ay * tstep
+        x = x + vx * tstep  
+        y = y + vy * tstep 
+        t += tstep
+        t_list.append(t)
+        x_list.append(x)
+        y_list.append(y)
+        vx_list.append(vx)
+        vy_list.append(vy)
+    
+    status = 'orbiting or escaped orbit'
     tarray = np.array(t_list)
     posarray = np.column_stack((x_list, y_list))
     varray = np.column_stack((vx_list, vy_list))
     return posarray, varray, tarray, status
 
 # call euler simulation function with initial y, end time, and time step
-def reentry(pos2, velo2, tstep = 0.1, max_steps = 100000):
+def reentry(pos2, velo2, tstep = 1.0, max_steps = 10000000):
     '''Takes final altitude, final velocity from orbital decay function, time intervals, and a max simulation duration, and calls the second_order_ex function to calculate the reentry position that will occur throughout the simulation run time
         returns the array of velocity, position, and time, for the simulation and whether or not the station hit the ground or remained in orbit, given the initial parameters'''
     # Parameters
-    initial_x_position = pos2[-1, 0] 
-    initial_y_position = pos2[-1, 1] 
-    x0 = initial_x_position
-    y0 = initial_y_position
-    x_initial_velocity = velo2[-1, 0] 
-    y_initial_velocity = velo2[-1, 1]
-    vx0 = x_initial_velocity
-    vy0 = y_initial_velocity
+    x0 = pos2[-1, 0]
+    y0 = pos2[-1, 1] 
+    vx0 = velo2[-1, 0]
+    vy0 = velo2[-1, 1]    
     pos, velo, ts, status = second_order_ex(x0, y0, vx0, vy0, tstep, max_steps)
     if pos is None:
         return None, None, None, status 
     return pos, velo, ts, status
-
-
     
